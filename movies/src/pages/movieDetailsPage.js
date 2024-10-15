4a7e2d6137923a1d3c37af1a0191969bfee17e58
tamen <20100902@mail.wit.ie>
2024-10-15 14:51:42 +0100

    Refactor: Template list page; HTTP API calls centralised

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -5,6 +5,7 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import React, {useState, useEffect}  from "react";
 import { useParams } from 'react-router-dom';
+import { getMovie, getMovieImages } from "../api/tmdb-api";
 
 const MoviePage = (props) => {
     const { id } = useParams();
@@ -12,26 +13,14 @@ const MoviePage = (props) => {
     const [images, setImages] = useState([]);
   
     useEffect(() => {
-      fetch(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-      )
-        .then((res) => {
-          return res.json();
-        })
-        .then((movie) => {
+      getMovie(id).then((movie) => {
           // console.log(movie)
           setMovie(movie);
         });
     }, [id]);
   
     useEffect(() => {
-      fetch(
-        `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-      )
-        .then((res) => res.json())
-        .then((json) => json.posters)
-        .then((images) => {
-          // console.log(images)
+      getMovieImages(id).then((images) => {
           setImages(images);
         });
         // eslint-disable-next-line
