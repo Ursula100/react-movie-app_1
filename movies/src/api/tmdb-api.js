5503355765cd5156c4ad56cd5d63283614834c43
tamen <20100902@mail.wit.ie>
2024-11-22 13:29:28 +0000

    Added fetch method: Get similar movies

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -118,3 +118,21 @@ export const getGenres = () => {
       throw error
    });
   };
+
+  export const getSimilarMovies = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
