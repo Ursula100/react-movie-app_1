34296f161d4754609efd82bdaa7a7af38a983077
tamen <20100902@mail.wit.ie>
2024-11-24 10:15:50 +0000

    Added person's movie credits to person's page

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -172,3 +172,21 @@ export const getGenres = () => {
       throw error
    });
   };
+
+  export const getPersonMovieCredits= ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
--- movies/src/pages/personPage.js
+++ movies/src/pages/personPage.js
@@ -1,27 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
-import Spinner from "../components/spinner";
-import PersonDetails from "../components/templatePeoplePage";
-import { getPersonBio } from "../api/tmdb-api";
+import Spinner from '../components/spinner';
+import PersonDetails from "../components/templatePersonPage";
+import { getPersonBio, getPersonMovieCredits } from "../api/tmdb-api";
 
 const PersonPage = () => {
   const { id } = useParams();
-  
-  const { data: person, error, isLoading, isError } = useQuery(
+
+  const { data: person, error: personError, isLoading: personLoading, isError: personIsError } = useQuery(
     ["person", { id }],
     getPersonBio
   );
 
-  if (isLoading) {
+  const { data: credits, error: creditsError, isLoading: creditsLoading, isError: creditsIsError } = useQuery(
+    ["credits", { id }],
+    getPersonMovieCredits
+  );
+
+  if (personLoading || creditsLoading) {
     return <Spinner />;
   }
 
-  if (isError) {
-    return <h1>{error.message}</h1>;
+  if (personIsError) {
+    return <h1>{personError.message}</h1>;
   }
 
-  return <PersonDetails person={person} />;
+  if (creditsIsError) {
+    return <h1>{creditsError.message}</h1>;
+  }
+
+  return <PersonDetails person={person} credits={credits} />;
 };
 
 export default PersonPage;
