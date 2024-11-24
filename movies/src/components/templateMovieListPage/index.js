017e040adc6a332e5ebfbcc0e5b29dfc63d6eba5
tamen <20100902@mail.wit.ie>
2024-11-24 18:41:15 +0000

    A few commments.

--- movies/src/components/templateMovieListPage/index.js
+++ movies/src/components/templateMovieListPage/index.js
@@ -12,15 +12,15 @@ function MovieListPageTemplate({ movies, title, action }) {
   const genreId = Number(genreFilter);
 
   let displayedMovies = movies
-    .filter((m) => m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1)
-    .filter((m) => genreId > 0 ? m.genre_ids.includes(genreId) : true);
+    .filter((m) => m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1) // checks if the movie's title contains the text entered in nameFilter, ignoring case.
+    .filter((m) => genreId > 0 ? m.genre_ids.includes(genreId) : true);//f a genre is selected (i.e., genreId > 0), it includes only the movies that have the selected genre ID in their genre_ids array. If no genre is selected (genreId === 0), it includes all movies
 
   if (sortFilter === "title") {
-    displayedMovies.sort((a, b) => a.title.localeCompare(b.title));
+    displayedMovies.sort((a, b) => a.title.localeCompare(b.title)); //Sorted alphabetically
   } else if (sortFilter === "release_date") {
-    displayedMovies.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
+    displayedMovies.sort((a, b) => new Date(b.release_date) - new Date(a.release_date)); //sorted in decreasing order
   } else if (sortFilter === "rating") {
-    displayedMovies.sort((a, b) => b.vote_average - a.vote_average);
+    displayedMovies.sort((a, b) => b.vote_average - a.vote_average); //sorted in decreasing order - diss<0, b comes first
   }
 
   const handleChange = (type, value) => {
